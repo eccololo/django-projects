ef54017a16ce96c7c33116240455904da55fef7c
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-01-09 03:19:17 +0100

    GIOS API integration successful - fixes #20.

--- Weather/weather/lookup/views.py
+++ Weather/weather/lookup/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-
-
-# Create your views here.
+import requests
+import json
 
 def view_home(request):
-    return render(request, template_name="home.html", context={})
+
+    # Air Quality APIs from GIOS in Poland.
+    gios_endpoint = "https://api.gios.gov.pl/pjp-api/rest/station/findAll"
+    response = requests.get(gios_endpoint, timeout=3)
+    print(response.content)
+    try:
+        api_content = json.loads(response.content)
+    except Exception as e:
+        api_content = {"error": e}
+
+    return render(request, template_name="home.html", context={
+        'api_content': api_content
+    })
 
 
 def view_about(request):
