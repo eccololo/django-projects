21ed98a38e15d8df0aababce7647612bb905c50e
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-01-13 15:41:01 +0100

    Add delete button to home page that can delete 1 ToDo item.

--- ToDo/todo_app/todo_list/urls.py
+++ ToDo/todo_app/todo_list/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.render_home, name="home_link"),
-    path('about', views.render_about, name="about_link")
+    path('about', views.render_about, name="about_link"),
+    path('delete/<list_id>', views.render_delete, name="delete_link")
 ]
--- ToDo/todo_app/todo_list/views.py
+++ ToDo/todo_app/todo_list/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import List
 from .forms import ListForm
+from django.http import HttpResponseRedirect
 
 
 def render_home(request):
-
     if request.method == "POST":
-        form_item = ListForm(request.POST  or None)
+        form_item = ListForm(request.POST or None)
 
         if form_item.is_valid():
             form_item.save()
@@ -28,3 +28,10 @@ def render_home(request):
 
 def render_about(request):
     return render(request, template_name="about.html", context={})
+
+
+def render_delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ("Item deleted"))
+    return redirect("home_link")
