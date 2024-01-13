04ec9aac8c41260d6b41f8fab5413265db94a022
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-01-13 12:57:12 +0100

    Add List model and register it in admin.py. Add search and filter functionality to admin panel.

--- ToDo/todo_app/todo_list/admin.py
+++ ToDo/todo_app/todo_list/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import List
 
-# Register your models here.
+
+class ListAdmin(admin.ModelAdmin):
+    fields = ["item", "completed"]
+    list_filter = ["completed"]
+    search_fields = ["item"]
+
+
+admin.site.register(List, ListAdmin)
--- ToDo/todo_app/todo_list/models.py
+++ ToDo/todo_app/todo_list/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class List(models.Model):
+    item = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.__get_item_title()
+
+    def __get_item_title(self):
+        completed = "[Unknown]"
+        if self.completed:
+            completed = "[Done]"
+        else:
+            completed = "[Undone]"
+
+        return f"{self.item} {completed}"
