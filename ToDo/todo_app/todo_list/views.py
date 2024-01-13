7344bb6b80accaff3aa8533d455a0af76966824d
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-01-13 14:03:27 +0100

    Add functionality that allow user to add todo item to todo list and update frontend home page.

--- ToDo/todo_app/todo_list/views.py
+++ ToDo/todo_app/todo_list/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from .models import List
+from .forms import ListForm
 
 
 def render_home(request):
-    all_items = List.objects.all()
-    return render(request, template_name="home.html",
-                  context={
-                      'all_items': all_items
-                  })
+
+    if request.method == "POST":
+        form_item = ListForm(request.POST  or None)
+
+        if form_item.is_valid():
+            form_item.save()
+
+            all_items = List.objects.all()
+
+            return render(request, template_name="home.html",
+                          context={
+                              'all_items': all_items
+                          })
+    else:
+        all_items = List.objects.all()
+        return render(request, template_name="home.html",
+                      context={
+                          'all_items': all_items
+                      })
+
 
 def render_about(request):
     return render(request, template_name="about.html", context={})
