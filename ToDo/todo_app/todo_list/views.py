f3695bb7ceb26c6497b1ad2a4a0abfbb400cd194
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-01-13 13:03:57 +0100

    Create few test items in List model. Displaying test items in home.html template on frontend.

--- ToDo/todo_app/todo_list/views.py
+++ ToDo/todo_app/todo_list/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import List
 
 
 def render_home(request):
-    return render(request, template_name="home.html", context={})
+    all_items = List.objects.all()
+    return render(request, template_name="home.html",
+                  context={
+                      'all_items': all_items
+                  })
 
 def render_about(request):
     return render(request, template_name="about.html", context={})
