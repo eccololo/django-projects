e5bb8a73a926bd6b4347ceadeb028f3166a64c5d
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-01-13 12:04:21 +0100

    Add basic templates and setting up basic rendering this templates in browser.

--- ToDo/todo_app/todo_list/urls.py
+++ ToDo/todo_app/todo_list/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.render_home, name="home_link"),
+    path('about', views.render_about, name="about_link")
 ]
--- ToDo/todo_app/todo_list/views.py
+++ ToDo/todo_app/todo_list/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def render_home(request):
+    return render(request, template_name="home.html", context={})
+
+def render_about(request):
+    return render(request, template_name="about.html", context={})
