e69204990ff12c207842395a9eb03d7aad51e7cf
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-02-20 18:13:43 +0100

    Add functionality to fetch stock data by search query by user input. Output stock info of a company to home page.

--- XStocks/xstocks/quotes/views.py
+++ XStocks/xstocks/quotes/views.py
@@ -4,15 +4,26 @@ import json
 
 
 def home(request):
-    public_token = "pk_47ea754a042d4a80afaae8e3b0ba39a4"
-    api_endpoint = f"https://api.iex.cloud/v1/data/core/quote/aapl?token={public_token}"
-    response = requests.get(api_endpoint)
-    try:
-        api_data = json.loads(response.content)
-    except Exception as e:
-        api_data = "Error ..."
 
-    return render(request, template_name="base.html", context={'api_data': api_data})
+    if request.method == "POST":
+        ticker = request.POST['ticker']
+        public_token = "pk_47ea754a042d4a80afaae8e3b0ba39a4"
+        api_endpoint = f"https://api.iex.cloud/v1/data/core/quote/{ticker}?token={public_token}"
+        response = requests.get(api_endpoint)
+        try:
+            api_data = json.loads(response.content)
+            api_data = api_data[0]
+            if api_data is None:
+                api_data = "Error"
+        except Exception as e:
+            api_data = "Error"
+        return render(request, template_name="base.html", context={'api_data': api_data})
+    else:
+        return render(request, template_name="base.html", context={'ticker': "Use search for finding stocks info."})
+
 
 def about(request):
+    return render(request, template_name="about.html", context={})
+
+def add_stock(request):
     return render(request, template_name="about.html", context={})
