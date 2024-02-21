d1c8d180137bec8073961e17345e95c5c082a396
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-02-21 16:56:06 +0100

    Add loading ajax spinner when data are loaded to the table of stocks portfolio.

--- XStocks/xstocks/quotes/static/js/script.js
+++ XStocks/xstocks/quotes/static/js/script.js
@@ -1,6 +1,18 @@
 $(function () {
-//    $(".table-toggle").hide()
-//    $(".toggle-portfolio").click(function() {
-//        $(".portfolio").toggle()
-//    });
+    const spinnerBox = $("#spinner-box");
+    const dataBox = $("#data-box");
+
+    $.ajax({
+        type: 'GET',
+        url: 'add_stock',
+        success: function(response) {
+            setTimeout(()=>{
+                spinnerBox.addClass("not-visible")
+                dataBox.removeClass("d-none")
+            }, 500)
+        },
+        error: function(error) {
+            console.log(error)
+        }
+    });
 });
