8619e203bacc989d1c2240b116e3a5bdad3ccfa6
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-02-23 18:48:52 +0100

    Create basic views and basic url path for basic templates in secureapp.

--- SecureUp/secureup/secureapp/views.py
+++ SecureUp/secureup/secureapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, template_name="index.html")
+
+def register(request):
+    return render(request, template_name="register.html")
+
+def dashboard(request):
+    return render(request, template_name="dashboard.html")
