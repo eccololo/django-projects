51446ef1b9c7a2be5a1aa98b4781b07867edf965
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-02-26 14:15:17 +0100

    Adding content to register view to service properly the user registration form.

--- SecureUp/secureup/secureapp/views.py
+++ SecureUp/secureup/secureapp/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
 
 def home(request):
     return render(request, template_name="index.html")
 
 def register(request):
-    return render(request, template_name="register.html")
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    context = {"form": form}
+    return render(request, template_name="register.html", context=context)
 
 def dashboard(request):
     return render(request, template_name="dashboard.html")
