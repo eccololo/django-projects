fecab1b6483af343e01c470a22b99b7b816900a1
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-03-15 23:04:26 +0100

    Add custom Django command that add functionality to export all data from any model to a CSV file.

--- Automat/dataentry/management/commands/exportdata.py
+++ Automat/dataentry/management/commands/exportdata.py
@@ -1,16 +1,34 @@
-from django.core.management.base import BaseCommand
-from dataentry.models import Student
+from django.core.management.base import BaseCommand, CommandError
+from django.apps import apps
 import csv
 import datetime
 import os
 
-# Proposed commmand - python manage.py exportdata
+# Proposed commmand - python manage.py exportdata <model-name>
 class Command(BaseCommand):
 
-    help = "Exports data from Student model to a CSV file."
+    help = "Exports data from database to a CSV file."
+
+    def add_arguments(self, parser):
+        parser.add_argument('model_name', type=str, help="Model name.")
 
     def handle(self, *args, **kwargs):
-        students = Student.objects.all()
+        model_name = kwargs['model_name'].capitalize()
+
+        # Search through all models.
+        model = None
+        for app_config in apps.get_app_configs():
+            try:
+                model = apps.get_model(app_config.label, model_name)
+                break
+            except LookupError:
+                continue
+
+        if not model:
+            raise CommandError(f"Model '{model_name}' not found in any app.")
+        else:
+              model_data = model.objects.all()
+
 
         timestamp = datetime.datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
         export_dir = os.path.join(os.getcwd(), "exports")
@@ -18,17 +36,17 @@ class Command(BaseCommand):
         if not os.path.exists(export_dir):
             os.mkdir(export_dir)
 
-        file_name = f"exported_students_data_{timestamp}.csv"
+        file_name = f"exported_{model_name}_data_{timestamp}.csv".lower()
         file_path = os.path.join(export_dir, file_name)
 
         with open(file_path, "w", newline="") as file:
             writer = csv.writer(file)
 
             # Writes CSV header
-            writer.writerow(['roll_no', 'name', 'age'])
+            writer.writerow([field.name for field in model._meta.fields])
 
             # Writes actual data
-            for student in students:
-                writer.writerow([student.roll_no, student.name, student.age])
+            for data in model_data:
+                writer.writerow([getattr(data, field.name) for field in model._meta.fields])
 
         self.stdout.write(self.style.SUCCESS("Data exported to CSV file successfully!"))
