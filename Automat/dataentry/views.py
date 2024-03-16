3445995d2bb7a6cb3eb3c934308c96a49aacee45
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-03-16 08:44:50 +0100

    Create function that returns custom models in list. Showing custom models list on importdata template.

--- Automat/dataentry/views.py
+++ Automat/dataentry/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .utils import get_all_custom_models
 
 def import_data(request):
-    return render(request, template_name="dataentry/importdata.html")
+    if request.method == "POST":
+        pass
+    else:
+        custom_models = get_all_custom_models()
+        context = {
+            'custom_models': custom_models
+        }
+    return render(request, template_name="dataentry/importdata.html", context=context)
