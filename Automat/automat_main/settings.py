3599d7523fbccf5337d0e345a5d411cde289cc87
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-03-16 11:50:01 +0100

    Add Jazzmin custom admin panel template to project.

--- Automat/automat_main/settings.py
+++ Automat/automat_main/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin', # For Jazzmin Admin Panel Template
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dataentry',
-    'uploads'
+    'uploads',
 ]
 
 MIDDLEWARE = [
