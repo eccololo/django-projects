5eb033343c51a978ae0b8da14328f446757dd5a3
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-03-16 08:46:06 +0100

    Create app uploads for uploading files purposes.

--- Automat/automat_main/settings.py
+++ Automat/automat_main/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dataentry'
+    'dataentry',
+    'uploads'
 ]
 
 MIDDLEWARE = [
