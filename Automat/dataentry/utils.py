7ab8351e1a13214ed15eae6e3660536ec8578b40
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-03-16 10:26:16 +0100

    Work on add import data from CSV file to DB functionality from UI.

--- Automat/dataentry/utils.py
+++ Automat/dataentry/utils.py
@@ -1,7 +1,7 @@
 from django.apps import apps
 
 def get_all_custom_models():
-    default_models = ["LogEntry", "Permission", "Group", "ContentType", "Session", "User"]
+    default_models = ["LogEntry", "Permission", "Group", "ContentType", "Session", "User", "Upload"]
     custom_models = []
 
     for model in apps.get_models():
--- Automat/dataentry/views.py
+++ Automat/dataentry/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.management import call_command
 from .utils import get_all_custom_models
+from uploads.models import Upload
+import platform
 
 def import_data(request):
     if request.method == "POST":
-        pass
+        file_path = request.FILES.get("file_path")
+        model_name = request.POST.get("model_name")
+
+        # Store file in Upload model.
+        upload = Upload.objects.create(file=file_path, model_name=model_name)
+
+        # Create relative path to uploaded file.
+        relative_file_path = str(upload.file.url)
+        base_url = str(settings.BASE_DIR)
+
+        # Path fix for Windows users.
+        if platform.system() == "Windows":
+            base_url = base_url.replace('\\', '/')
+
+        full_file_path = base_url + relative_file_path
+
+        # Trigger the data import command.
+        # FIXME:
+        # 1. During calling this command error occures.
+        try:
+            call_command('importdata', full_file_path, model_name)
+        except Exception as e:
+            raise e
+
+        return redirect("import_data")
     else:
         custom_models = get_all_custom_models()
         context = {
