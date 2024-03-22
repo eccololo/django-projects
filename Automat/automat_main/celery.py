6b56a5e05a9d7f2da95c56980cfea0b8d6359765
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-03-22 15:36:00 +0100

    Add configuration for redis and celery.

--- Automat/automat_main/celery.py
+++ Automat/automat_main/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automat_main.settings')
 
 # Set app new Celery app for our Django project.
 app = Celery('automat_main')
