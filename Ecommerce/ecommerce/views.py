4c2e70dd2a8224b3107022681ceb1a5819b46229
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-04-23 13:56:25 +0200

    Add code to run home page template in ecommerce project.

--- Ecommerce/ecommerce/views.py
+++ Ecommerce/ecommerce/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("This is home page.")
+    return render(request, "home.html")
