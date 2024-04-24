234cbedf4cb5d77ba55cf258cdabbbfeed6e1384
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-04-24 20:31:30 +0200

    Create Category model. Register it in admin. Create migrations and superuser. Add model name confirgurations.

--- Ecommerce/category/models.py
+++ Ecommerce/category/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to="photos/categories/", blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
