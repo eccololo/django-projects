1299869c70e5738cbae9809bfa065d6b0a6dfe99
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-04-30 18:18:22 +0200

    Add auto-generate slug in Category model in admin panel. Add some demo categories with media files.

--- Ecommerce/category/models.py
+++ Ecommerce/category/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
         verbose_name_plural = "categories"
 
     name = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=255, blank=True)
     cat_image = models.ImageField(upload_to="photos/categories/", blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
