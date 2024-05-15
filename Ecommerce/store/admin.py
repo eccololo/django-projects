8abce7b1cdbc7dbd3002533b97d8d672877f4ac7
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-05-15 15:16:03 +0200

    Create model Product. Register model Product in admin.

--- Ecommerce/store/admin.py
+++ Ecommerce/store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {
+        "slug": ["product_name"]
+    }
+
+    list_display = ["product_name", "price", "category", "stock", "is_available", "modified_date"]
+    list_filter = ["price"]
+    search_fields = ["product_name", "description"]
+
+admin.site.register(Product, ProductAdmin)
+
--- Ecommerce/store/models.py
+++ Ecommerce/store/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from category.models import Category
 
-# Create your models here.
+class Product(models.Model):
+
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=500, blank=True)
+    price = models.IntegerField()
+    images = models.ImageField(upload_to='photos/products/')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    #Relation
+    # FIXME:
+    # 1. Change on_delete - when Category will be deleted it will dissapear from product
+    #    without deleting it.
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.product_name
