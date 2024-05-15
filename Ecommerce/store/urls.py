5044e37990b981f919efdde4f444793ac6b88589
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-05-15 17:07:42 +0200

    Add FIXME item to urls.py.

--- Ecommerce/store/urls.py
+++ Ecommerce/store/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.store, name="store"),
+    # FIXME:
+    # This url doesn't work.
     path("<slug:category_slug>", views.store, name="product_by_category")
 ]
