f015aae7c0703ae32a36587f1ce7df59257fe33f
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-05-22 18:03:29 +0200

    Make category links in navbar functional.

--- Ecommerce/store/urls.py
+++ Ecommerce/store/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.store, name="store"),
-    # FIXME:
-    # This url doesn't work.
     path("<slug:category_slug>", views.store, name="product_by_category")
 ]
