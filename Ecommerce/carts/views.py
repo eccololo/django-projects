6045aa13ab48ef24f93b2d4bed704c9eefb41089
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-05-25 15:04:43 +0200

    Add cart view function in views.py.

--- Ecommerce/carts/views.py
+++ Ecommerce/carts/views.py
@@ -3,11 +3,6 @@ from django.http import HttpResponse
 from store.models import Product
 from .models import Cart, CartItem
 
-def cart(request):
-    return render(request, 'store/cart.html')
-
-
-
 def _cart_id(request):
     cart = request.session.session_key
     if not cart:
@@ -40,5 +35,24 @@ def add_cart(request, product_id):
         )
     
     cart_item.save()
-    return HttpResponse(cart_item.product)
-    # return redirect("cart")
+    return redirect("cart")
+
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total = (cart_item.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectNotExists:
+        pass
+        
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items
+    }    
+    return render(request, 'store/cart.html', context)
+
+
